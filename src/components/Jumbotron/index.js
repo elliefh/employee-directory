db035f5f7b7f22d12e7a22a7365a6bdf620eab6e
elliefh <elliefu18@gmail.com>
2021-03-16 15:53:37 -0700

    removed unnecessary comments and p tags

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,14 +1,9 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron"
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function MyJumbotron(props) {
     return(<Jumbotron>
         <h1>Employee Directory</h1>
-        <p>
-        </p>
-        <p>
-            {/* <Button variant="primary">Learn more</Button> */}
-        </p>
     </Jumbotron>    
     )
 }
