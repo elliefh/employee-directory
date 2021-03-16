48f935f38f53dce672995c8f28debce3a11655a2
elliefh <elliefu18@gmail.com>
2021-03-16 16:18:57 -0700

    added gender category and filter by gender button

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Table from "react-bootstrap/Table"
+import Table from "react-bootstrap/Table";
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function MyTable(props) {
 return(
@@ -8,15 +11,25 @@ return(
     <thead>
         <tr>
         <th class="align-middle">Image</th>
-        <th>
+        <th class="align-middle">
             First Name
             <Button variant="outline-secondary btn-sm m-2">Sort</Button>
         </th>
-        <th>Last Name
-        <Button variant="outline-secondary btn-sm m-2">Sort</Button>
+        <th class="align-middle">
+            Last Name
+            <Button variant="outline-secondary btn-sm m-2">Sort</Button>
         </th>
         <th class="align-middle">Email</th>
         <th class="align-middle">State</th>
+        <th class="align-middle">
+            Gender
+            <ButtonGroup>
+                <DropdownButton as={ButtonGroup} title="Filter By" id="bg-nested-dropdown" variant="outline-secondary btn-sm m-2">
+                <Dropdown.Item eventKey="1">male</Dropdown.Item>
+                <Dropdown.Item eventKey="2">female</Dropdown.Item>
+                </DropdownButton>
+            </ButtonGroup>
+        </th>
         </tr>
     </thead>
     <tbody>
