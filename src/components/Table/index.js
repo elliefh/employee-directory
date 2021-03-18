11bbf61019af20b2a3a83cf09e778e2c26225f4e
elliefh <elliefu18@gmail.com>
2021-03-18 15:03:09 -0700

    added props.onclick to sort first and last names

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,36 +6,38 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
 
 function MyTable(props) {
-return(
-    <Table striped bordered hover>
-    <thead>
-        <tr>
-        <th class="align-middle">Image</th>
-        <th class="align-middle">
-            First Name
-            <Button variant="outline-secondary btn-sm m-2">Sort</Button>
-        </th>
-        <th class="align-middle">
-            Last Name
-            <Button variant="outline-secondary btn-sm m-2">Sort</Button>
-        </th>
-        <th class="align-middle">Email</th>
-        <th class="align-middle">State</th>
-        <th class="align-middle">
-            Gender
-            <ButtonGroup>
-                <DropdownButton as={ButtonGroup} title="Filter By" id="bg-nested-dropdown" variant="outline-secondary btn-sm m-2">
-                <Dropdown.Item eventKey="1">male</Dropdown.Item>
-                <Dropdown.Item eventKey="2">female</Dropdown.Item>
-                </DropdownButton>
-            </ButtonGroup>
-        </th>
-        </tr>
-    </thead>
-    <tbody>
-        {props.children}
-    </tbody>
-    </Table>
-)}
+    return(
+        <Table striped bordered hover>
+        <thead>
+            <tr>
+            <th class="align-middle">Image</th>
+            <th class="align-middle">
+                First Name
+                <Button variant="outline-secondary btn-sm m-2" onClick={props.firstnamesort}>Sort</Button>
+            </th>
+            <th class="align-middle">
+                Last Name
+                <Button variant="outline-secondary btn-sm m-2" onClick={props.lastnamesort}>Sort</Button>
+            </th>
+            <th class="align-middle">Email</th>
+            <th class="align-middle">State</th>
+            <th class="align-middle">
+                Gender
+                <ButtonGroup>
+                    <DropdownButton as={ButtonGroup} title="Filter By" id="bg-nested-dropdown" variant="outline-secondary btn-sm m-2">
+                    <Dropdown.Item eventKey="1">male</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">female</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">all</Dropdown.Item>
+                    </DropdownButton>
+                </ButtonGroup>
+            </th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.children}
+        </tbody>
+        </Table>
+    )
+}
 
 export default MyTable;
