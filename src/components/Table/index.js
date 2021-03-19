a5805dcd27f5a17f5bc649df56f8f7a8189be0dd
elliefh <elliefu18@gmail.com>
2021-03-18 19:42:57 -0700

    added onclick tags for filter methods

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -10,24 +10,24 @@ function MyTable(props) {
         <Table striped bordered hover>
         <thead>
             <tr>
-            <th class="align-middle">Image</th>
-            <th class="align-middle">
+            <th className="align-middle">Image</th>
+            <th className="align-middle">
                 First Name
                 <Button variant="outline-secondary btn-sm m-2" onClick={props.firstnamesort}>Sort</Button>
             </th>
-            <th class="align-middle">
+            <th className="align-middle">
                 Last Name
                 <Button variant="outline-secondary btn-sm m-2" onClick={props.lastnamesort}>Sort</Button>
             </th>
-            <th class="align-middle">Email</th>
-            <th class="align-middle">State</th>
-            <th class="align-middle">
+            <th className="align-middle">Email</th>
+            <th className="align-middle">State</th>
+            <th className="align-middle">
                 Gender
                 <ButtonGroup>
                     <DropdownButton as={ButtonGroup} title="Filter By" id="bg-nested-dropdown" variant="outline-secondary btn-sm m-2">
-                    <Dropdown.Item eventKey="1">male</Dropdown.Item>
-                    <Dropdown.Item eventKey="2">female</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">all</Dropdown.Item>
+                    <Dropdown.Item eventKey="male" onClick={props.filtermale}>male</Dropdown.Item>
+                    <Dropdown.Item eventKey="female" onClick={props.filterfemale}>female</Dropdown.Item>
+                    <Dropdown.Item eventKey="all" onClick={props.nofilter}>all</Dropdown.Item>
                     </DropdownButton>
                 </ButtonGroup>
             </th>
